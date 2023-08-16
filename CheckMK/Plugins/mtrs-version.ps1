0e1537acda11c7d27b9a4ccb4c1c338892057bc3
Matias Bernhardt <matias.bernhardt@gmail.com>
2023-08-16 08:15:13 +0200

    Update mtrs-version.ps1
    Added Hearthbeat and Hardware checks.

--- CheckMK/Plugins/mtrs-version.ps1
+++ CheckMK/Plugins/mtrs-version.ps1
@@ -1,4 +1,4 @@
-function Out-CheckkMK-String {
+function Out-CheckkMK-SimpleValue {
     param (
         [Parameter(Mandatory)]
 		[string]$Name,
@@ -8,11 +8,12 @@ function Out-CheckkMK-String {
 		[int]$Status
     )
 
-    [string]$returnstring = "$Status" + ' "' + $Name + '"' + " - " + "$Value"
+    [string]$returnstring = "$Status" + ' "' + $Name + '"' + " - " + $Value
 
     return $returnstring
 }
 
+# Get MTRS Version
 
 try {$app = Get-AppxPackage -AllUsers *SkypeRoom*
     $version = $app.version
@@ -23,5 +24,48 @@ catch {
     $status = 3
 }
 finally {
-    Out-CheckkMK-String -Name "MTRS Version" -Value $version -Status $status
+    Out-CheckkMK-SimpleValue -Name "MTRS Version" -Value $version -Status $status
 }
+
+
+
+$LogName = "Skype Room System"
+
+# Get Event Log (At App Start)
+$restartEvent = Get-WinEvent -FilterHashtable @{ LogName=$logname; ID=4000;} -ErrorAction SilentlyContinue | Select-Object -First 1
+$restartMsg =  $restartEvent.Message -replace '{"' -replace '"}','"' -replace '":"', '="' -replace '","',"`"`n"
+$restartMsg = ConvertFrom-StringData -StringData $restartMsg
+
+if (($restartMsg.Alias -eq $null) -or ($restartMsg.Alias -eq "")) {
+    $status = 2
+}
+else {
+    $status = 0
+}
+
+Out-CheckkMK-SimpleValue -Name "MTRS Account" -Value (($restartMsg.Alias).Replace("`"","")) -Status $status
+
+
+# Heartbeat status
+$heartbeatEvent = Get-WinEvent -FilterHashtable @{ LogName=$logname; ID=2000,2001;} -ErrorAction SilentlyContinue | Select-Object -First 1
+
+if ($heartbeatEvent.Id -eq 2000) {$status = 0} #Healthy
+elseif ($heartbeatEvent.Id -eq 2001) {$status = 2} #Error
+else {$status = 3} # Unknown
+
+
+$heartbeatMsg =  $heartbeatEvent.Message -replace '{"' -replace '"}','"' -replace '":"', '="' -replace '","',"`"`n"
+$heartbeatMsg = ConvertFrom-StringData -StringData $heartbeatMsg
+Out-CheckkMK-SimpleValue -Name "MTRS Heartbeat" -Value (($heartbeatMsg.Description).Replace("`"","")) -Status $status
+
+# Hardware status
+$hardwareEvent = Get-WinEvent -FilterHashtable @{ LogName=$logname; ID=3000,3001;} -ErrorAction SilentlyContinue | Select-Object -First 1
+
+if ($hardwareEvent.Id -eq 3000) {$status = 0} #Healty
+elseif ($hardwareEvent.Id -eq 3001) {$status = 2} #Error
+else {$status = 3} # Unknown
+
+
+$hardwareMsg =  $hardwareEvent.Message -replace '{"' -replace '"}','"' -replace '":"', '="' -replace '","',"`"`n"
+$hardwareMsg = ConvertFrom-StringData -StringData $hardwareMsg
+Out-CheckkMK-SimpleValue -Name "MTRS Hardware" -Value (($hardwareMsg.Description).Replace("`"","")) -Status $status
